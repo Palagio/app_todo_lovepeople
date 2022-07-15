b64727f7abc359dcdf8c101d3d7017c1315ad07d
Palagio <joaogabriel7499@gmail.com>
2022-07-15 19:21:24 -0300

    delet some pages

--- lib/modules/home/new_task/add_new_task_controller.dart
+++ lib/modules/home/new_task/add_new_task_controller.dart
@@ -1,5 +1,5 @@
 import 'package:app_todo_lovepeople/modules/home/new_task/new_task_model.dart';
-import 'package:app_todo_lovepeople/modules/home/new_task/repository/new_task_repository.dart';
+
 import 'package:app_todo_lovepeople/modules/home/new_task/repository/new_task_repository_impl.dart';
 import 'package:flutter/cupertino.dart';
 
