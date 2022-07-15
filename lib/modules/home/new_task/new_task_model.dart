b34534c42a358205faa78dbf95c982e68ba2287b
Palagio <joaogabriel7499@gmail.com>
2022-07-15 14:56:49 -0300

    add newtaskmodel

--- lib/modules/home/new_task/new_task_model.dart
+++ lib/modules/home/new_task/new_task_model.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 class NewTaskModel {
-  String? title;
-  String? description;
-  Color? color;
+  final String? title;
+  final String? description;
+  final Color? color;
   NewTaskModel({
-    required this.title,
-    required this.description,
-    required this.color,
+    this.title,
+    this.description,
+    this.color,
   });
+
+  
 }
